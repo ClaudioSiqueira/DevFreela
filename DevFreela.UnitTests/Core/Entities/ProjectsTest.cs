80ec1a27a22f33096f025daaf7396cda492fa2ff
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2022-04-08 00:16:34 -0300

    Adding Unit Test with xUnit!

---
+++ DevFreela.UnitTests/Core/Entities/ProjectsTest.cs
@@ -0,0 +1,34 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectsTest
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Titulo de teste", "Descrição de teste", 1, 1, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
